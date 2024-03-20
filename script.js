fa7e2daeded5c34c15b18a921f59d6ac509ea1ee
ya-hs <68033139+ya-hs@users.noreply.github.com>
2024-03-20 14:55:46 +0000

    :art: Improve structure of functions

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ const btnNum = document.querySelectorAll(".btn-num");
 const btnOp = document.querySelectorAll(".btn-op");
 const btnEquals = document.querySelector("#btn-equals");
 const btnSelector = document.querySelectorAll("button");
-const btnAdd = document.querySelector("#btn-add");
+
+let displayText = "";
+let firstNumber = "";
+let secondNumber = "";
 
 
 // ADD CLICKED NUMBER TO DISPLAY
 
-let displayText = "";
+
 display.textContent = displayText;
 
 function addToDisplay(numClicked) {
@@ -25,14 +28,14 @@ btnNum.forEach(button => {
 
 // FUNCTIONS
 
-let firstNumber = "";
-let secondNumber = "";
+btnEquals.addEventListener('click', () => {
+    performCalculation();
+})
+
 
 btnOp.forEach(button => {
     button.addEventListener('click', () => {
         clearNums();
-        console.log(operate(3, 6, button.id));
-        console.log(firstNumber);
         return chosenOperator = button.id;
     })
 })
@@ -43,27 +46,25 @@ function clearNums() {
     display.textContent = displayText;
 }
 
-function operate(num1, num2, operator) {
-
-    if (operator == "btn-add") {
-        return add(num1, num2);
-    } else if (operator == "btn-subtract") {
-        return subtract(num1, num2);
-    } else if (operator == "btn-multiply") {
-        return multiply(num1, num2);
-    } else if (operator == "btn-divide") {
-        return divide(num1, num2);
-    } else if (operator == "btn-square") {
-        return square(num1);
-    }
+function performCalculation() {
+    const secondNumber = displayText;
+    const result = operate(firstNumber, secondNumber, chosenOperator);
+    displayText = result;
+    display.textContent = result;
+    console.log(operate(firstNumber, secondNumber, chosenOperator));
 }
 
-btnEquals.addEventListener('click', () => {
-    secondNumber = displayText;
-    displayText = operate(firstNumber, secondNumber, chosenOperator);
-    display.textContent = displayText;
-    console.log(operate(firstNumber, secondNumber, chosenOperator));
-})
+function operate(num1, num2, operator) {
+    const operations = {
+        "btn-add": add,
+        "btn-subtract": subtract,
+        "btn-multiply": multiply,
+        "btn-divide": divide,
+        "btn-square": square
+    };
+    return operations[operator](num1, num2);
+}
+
 
 
 
@@ -87,13 +88,3 @@ function divide(num1, num2) {
 function square(num) {
     return Number(num) ** 2;
 }
-
-
-// TESTING
-
-console.log(add(6, 3));
-console.log(subtract(6, 3));
-console.log(multiply(6, 3));
-console.log(divide(6, 3));
-console.log(square(3));
-console.log(operate(3, 7, "add"));
