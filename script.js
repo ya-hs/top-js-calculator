a6678136d074e0d9bb4d5ec7cfed2ba96cd44413
ya-hs <68033139+ya-hs@users.noreply.github.com>
2024-03-20 13:57:52 +0000

    :sparkles: Add function to display selected numbers

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
 const display = document.querySelector("#display");
+const btnNum = document.querySelectorAll(".btn-num");
 const btnSelector = document.querySelectorAll("button");
-let firstNum
-let operator
-let secondNum
-
-// const btnClear = document.querySelector("#btn-clear");
-// const btnBackspace = document.querySelector("#btn-back");
-// const btnSquare = document.querySelector("#btn-square");
-// const btnDivide = document.querySelector("#btn-divide");
-// const btnSeven = document.querySelector("#btn-7");
-// const btnEight = document.querySelector("#btn-8");
-// const btnNine = document.querySelector("#btn-9");
-// const btnMultiply = document.querySelector("#btn-multiply");
-// const btnFour = document.querySelector("#btn-4");
-// const btnFive = document.querySelector("#btn-5");
-// const btnSix = document.querySelector("#btn-6");
-// const btnSubtract = document.querySelector("#btn-subtract");
-// const btnOne = document.querySelector("#btn-1");
-// const btnTwo = document.querySelector("#btn-2");
-// const btnThree = document.querySelector("#btn-3");
-// const btnAdd = document.querySelector("#btn-add");
-// const btnPoint = document.querySelector("#btn-point");
-// const btnZero = document.querySelector("#btn-0");
-// const btnEquals = document.querySelector("#btn-equals");
-
 
 
 // ARITHMETIC FUNCTIONS
@@ -61,13 +38,21 @@ function operate(num1, num2, operator) {
     }
 }
 
-// EVENT LISTENERS
+// ADD CLICKED NUMBER TO DISPLAY
 
-btnSelector.forEach(button => {
+let displayText = "";
+display.textContent = displayText;
+
+function addToDisplay(numClicked) {
+    displayText += numClicked;
+    display.textContent = displayText;
+}
+
+btnNum.forEach(button => {
     button.addEventListener('click', () => {
-        console.log(operate(3, 6, "add"));
-    });
-});
+        addToDisplay(button.innerText);
+    })
+})
 
 
 // TESTING
