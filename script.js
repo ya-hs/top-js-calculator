c7d4fd767eced2193c74baaaf1762b1c0f2e1f99
ya-hs <68033139+ya-hs@users.noreply.github.com>
2024-03-20 14:41:28 +0000

    :hammer: Add arithmetic functionality

--- script.js
+++ script.js
@@ -1,42 +1,10 @@
 const display = document.querySelector("#display");
 const btnNum = document.querySelectorAll(".btn-num");
+const btnOp = document.querySelectorAll(".btn-op");
+const btnEquals = document.querySelector("#btn-equals");
 const btnSelector = document.querySelectorAll("button");
+const btnAdd = document.querySelector("#btn-add");
 
-
-// ARITHMETIC FUNCTIONS
-
-function add(num1, num2) {
-    return num1 + num2;
-}
-
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    return num1 / num2;
-}
-function square(num) {
-    return num ** 2;
-}
-
-function operate(num1, num2, operator) {
-    if (operator == "add") {
-        return add(num1, num2);
-    } else if (operator == "sub") {
-        return subtract(num1, num2);
-    } else if (operator == "multi") {
-        return multiply(num1, num2);
-    } else if (operator == "div") {
-        return divide(num1, num2);
-    } else if (operator == "squ") {
-        return square(num1);
-    }
-}
 
 // ADD CLICKED NUMBER TO DISPLAY
 
@@ -53,6 +21,72 @@ btnNum.forEach(button => {
         addToDisplay(button.innerText);
     })
 })
+
+
+// FUNCTIONS
+
+let firstNumber = "";
+let secondNumber = "";
+
+btnOp.forEach(button => {
+    button.addEventListener('click', () => {
+        clearNums();
+        console.log(operate(3, 6, button.id));
+        console.log(firstNumber);
+        return chosenOperator = button.id;
+    })
+})
+
+function clearNums() {
+    firstNumber = displayText;
+    displayText = "";
+    display.textContent = displayText;
+}
+
+function operate(num1, num2, operator) {
+
+    if (operator == "btn-add") {
+        return add(num1, num2);
+    } else if (operator == "btn-subtract") {
+        return subtract(num1, num2);
+    } else if (operator == "btn-multiply") {
+        return multiply(num1, num2);
+    } else if (operator == "btn-divide") {
+        return divide(num1, num2);
+    } else if (operator == "btn-square") {
+        return square(num1);
+    }
+}
+
+btnEquals.addEventListener('click', () => {
+    secondNumber = displayText;
+    displayText = operate(firstNumber, secondNumber, chosenOperator);
+    display.textContent = displayText;
+    console.log(operate(firstNumber, secondNumber, chosenOperator));
+})
+
+
+
+// ARITHMETIC FUNCTIONS
+
+function add(num1, num2) {
+    return Number(num1) + Number(num2);
+}
+
+function subtract(num1, num2) {
+    return Number(num1) - Number(num2);
+}
+
+function multiply(num1, num2) {
+    return Number(num1) * Number(num2);
+}
+
+function divide(num1, num2) {
+    return Number(num1) / Number(num2);
+}
+function square(num) {
+    return Number(num) ** 2;
+}
 
 
 // TESTING
